7a72658af01b3ea0dcc18d6f942e4e96d340b138
whizygeek <pratik.hingu123@gmail.com>
2022-11-10 23:54:42 +0100

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/batman/robomechtrix_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/batman/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/batman/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/trixy/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/batman/robomechtrix_ws/src/trixy
+# Build directory: /home/batman/robomechtrix_ws/build/trixy
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
